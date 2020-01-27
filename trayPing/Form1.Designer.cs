6f966cf1e8a1ab653eab0aa4a2b9b220eea79f29
lvo <vlobarev@gmail.com>
2020-01-27 09:00:58 +0500

    first commit

---
+++ trayPing/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace trayPing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.host = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.removeFromListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Size = new System.Drawing.Size(382, 360);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(382, 25);
+            this.splitContainer2.SplitterDistance = 254;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 33);
+            this.textBox1.TabIndex = 0;
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.button1);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.button2);
+            this.splitContainer3.Size = new System.Drawing.Size(124, 25);
+            this.splitContainer3.SplitterDistance = 72;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "add host";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button2.Location = new System.Drawing.Point(0, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(48, 25);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "ping";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.host,
+            this.status});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(382, 331);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // host
+            // 
+            this.host.Text = "Host";
+            this.host.Width = 297;
+            // 
+            // status
+            // 
+            this.status.Text = "Status";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.removeFromListToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(220, 34);
+            // 
+            // removeFromListToolStripMenuItem
+            // 
+            this.removeFromListToolStripMenuItem.Name = "removeFromListToolStripMenuItem";
+            this.removeFromListToolStripMenuItem.Size = new System.Drawing.Size(219, 30);
+            this.removeFromListToolStripMenuItem.Text = "Remove from list";
+            this.removeFromListToolStripMenuItem.Click += new System.EventHandler(this.removeFromListToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Warning;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 360);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Pinger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader host;
+        private System.Windows.Forms.ColumnHeader status;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem removeFromListToolStripMenuItem;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ trayPing/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+
+using System.IO;
+
+namespace trayPing
+{
+    public partial class Form1 : Form
+    {
+        private void changeStatus(System.String status)
+        {
+            switch (status)
+            {
+                case "Error":
+                    notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+                    notifyIcon1.Icon = Properties.Resources.red;
+                    break;
+                case "Success":
+                    notifyIcon1.Text = "Все сервера доступны";
+                    notifyIcon1.Icon = Properties.Resources.green;
+                    break;
+                case "Start":
+                    button2.Text = "Stop";
+                    break;
+                default:
+                    notifyIcon1.Text = "Waiting for ping start";
+                    notifyIcon1.Icon = Properties.Resources.gray;
+                    button2.Text = "Ping";
+                    break;
+            }
+        }
+
+        private void pingList()
+        {
+            foreach (ListViewItem element in listView1.Items)
+            {
+                bool result = tryping(element.SubItems[0].Text);
+            }
+        }
+
+        private void PingCompletedCallback(object sender, PingCompletedEventArgs e)
+        {
+            bool pinging = false;
+            System.String host = e.UserState.ToString();
+            if (e.Cancelled)
+            {
+                //MessageBox.Show("Cancelled", "Cancelled");
+            }
+            else if (e.Error != null)
+            {
+                //MessageBox.Show(e.Error.ToString(), "Error");
+            }
+            else if (e.Reply.Status == IPStatus.Success)
+            {
+                //MessageBox.Show(host, "good");
+                pinging = true;
+            }
+            else
+            {
+                //MessageBox.Show("alarm", "bad");
+            }
+
+            foreach (ListViewItem element in listView1.Items)
+            {
+                if (element.SubItems[0].Text == host)
+                {
+                    element.SubItems[1].Text = "" + pinging;
+
+                    if (!pinging)
+                    {
+                        //notifyIcon1.BalloonTipTitle = "Внимание!";
+                        //notifyIcon1.BalloonTipText = "Нет связи с " + host + "!";
+                        //notifyIcon1.ShowBalloonTip(1000);
+                    }
+                }
+            }
+        }
+
+        private bool tryping(System.String host)
+        {
+            bool pinging = false;
+            
+            try
+            {
+                Ping pingSender = new Ping();
+
+                AutoResetEvent waiter = new AutoResetEvent(false);
+                pingSender.PingCompleted += new PingCompletedEventHandler(PingCompletedCallback);
+                pingSender.SendAsync(host, host);
+            }
+            catch
+            {
+                //return false;
+            }
+            
+
+            return pinging;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                changeStatus("Default");
+                timer1.Stop();
+                timer2.Stop();
+            }
+            else
+            {
+                changeStatus("Start");
+                pingList();
+                timer1.Start();
+                timer2.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pingList();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = textBox1.Text;
+            lvi.SubItems.Add("");
+            listView1.Items.Add(lvi);
+        }
+
+        private void removeFromListToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count > 0) {
+                listView1.Items.Remove(listView1.SelectedItems[0]);
+            }
+        }
+
+        private void notifyIcon1_Click(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.ShowInTaskbar = true;
+                this.WindowState = FormWindowState.Normal;
+            } else
+            {
+                this.Hide();
+                this.ShowInTaskbar = false;
+                this.WindowState = FormWindowState.Minimized;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           try {
+                var logFile = File.ReadAllLines("\\\\elem-dc0\\SYSVOL\\elem.ru\\scripts\\pinger\\pinger.cfg");
+                List<string> LogList = new List<string>(logFile);
+
+                foreach (System.String host in LogList)
+                {
+                    ListViewItem lvi = new ListViewItem();
+                    lvi.Text = host;
+                    lvi.SubItems.Add("");
+                    listView1.Items.Add(lvi);
+                }
+
+                this.Hide();
+                this.ShowInTaskbar = false;
+                this.WindowState = FormWindowState.Minimized;
+                changeStatus("Start");
+                pingList();
+                timer1.Start();
+                timer2.Start();
+            }
+            catch {
+                
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                //notifyIcon1.BalloonTipTitle = "Программа была спрятана";
+                //notifyIcon1.BalloonTipText = "Обратите внимание что программа была спрятана в трей и продолжит свою работу.";
+                //notifyIcon1.ShowBalloonTip(1000);
+                this.Hide();
+                this.ShowInTaskbar = false;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            bool status = true;
+            System.String noping = "Нет связи с ";
+            foreach (ListViewItem element in listView1.Items)
+            {
+                if (element.SubItems[1].Text == "False")
+                {
+                    status = false;
+                    noping += element.SubItems[0].Text + ", ";
+                }
+            }
+            if (status) {
+                changeStatus("Success");
+            } else
+            {
+                changeStatus("Error");
+                notifyIcon1.Text = noping;
+            }
+        }
+    }
+}
